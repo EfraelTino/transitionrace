31f92e2e412567761656ec1b67ef3f90a4a9a3be
efrael <efrael2001@gmail.com>
2023-04-18 18:48:35 +0200

    s

--- logica/conexion.php
+++ logica/conexion.php
@@ -11,11 +11,11 @@
    }
    */
    $host = "localhost";
-   // $user = "desarro1_transitionrun";
-   $user = "root";
-   // $pass = "_BTkOL.R^PfP";
-   $pass = "";
-   // $database = "desarro1_transitionrun";
+   $user = "desarro1_transitionrun";
+   // $user = "root";
+   $pass = "_BTkOL.R^PfP";
+   // $pass = "";
+   $database = "desarro1_transitionrun";
    $database = "transitionrun";
    $dblink = mysqli_connect($host, $user, $pass, $database);
    if($dblink){

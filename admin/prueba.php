6c4ad29c733333e0ec1a8957736424472483c008
efrael <efrael2001@gmail.com>
2023-04-20 04:55:10 +0200

    s

---
+++ admin/prueba.php
@@ -0,0 +1,9 @@
+<?php
+include "include/conexion.php";
+include "include/verificar_sesion.php";
+
+$sql = "select * FROM clientestra WHERE documento != '' ORDER BY puntuacion DESC"
+
+
+?>
+
--- admin/usuarios_registrado.php
+++ admin/usuarios_registrado.php
@@ -38,42 +38,50 @@ include "include/verificar_sesion.php";
       <!-- Menu en la parte superior -->
       <!-- page content -->
       <style>
-                    thead{
-                      background-color: #2C0341 !important;
-                      color:#fff !important;
-                    }
-                    tbody, td{
-                      background-color: #371441 !important;
-                      color: #fff !important;
-                    }
-                    .dataTables_length label, .dataTables_filter label{
-                      color: #fff !important;
-                    }
-                    .dataTables_length label select{
-                      background-color: #2C0341 !important;
-                      color: #fff !important;
-                    }
-                    .dataTables_filter label input{
-                      background-color: #2C0341 !important;
-                    }
-                    .dataTables_info{
-                      color: #fff !important;
-                    }
-                    #example_previous a , #example_next a{
-                      background: #593662 !important;
-                      color: #fff !important;
-                      border-color: #2C0341 !important;
-                    }
-                    .paginate_button  a{
-                      color: #fff;
-                      border: 0px;
-                      border-color: #2C0341 !important;
-                      background-color: #2C0341 !important;
+        thead {
+          background-color: #2C0341 !important;
+          color: #fff !important;
+        }
 
-                    }
+        tbody,
+        td {
+          background-color: #371441 !important;
+          color: #fff !important;
+        }
 
+        .dataTables_length label,
+        .dataTables_filter label {
+          color: #fff !important;
+        }
 
-                  </style>
+        .dataTables_length label select {
+          background-color: #2C0341 !important;
+          color: #fff !important;
+        }
+
+        .dataTables_filter label input {
+          background-color: #2C0341 !important;
+        }
+
+        .dataTables_info {
+          color: #fff !important;
+        }
+
+        #example_previous a,
+        #example_next a {
+          background: #593662 !important;
+          color: #fff !important;
+          border-color: #2C0341 !important;
+        }
+
+        .paginate_button a {
+          color: #fff;
+          border: 0px;
+          border-color: #2C0341 !important;
+          background-color: #2C0341 !important;
+
+        }
+      </style>
       <div class="right_col" role="main" style="background-color: #593662 !important;">
         <div class="row">
           <div class="col-md-12 col-sm-12 col-xs-12">
@@ -85,7 +93,7 @@ include "include/verificar_sesion.php";
               <div class="x_content">
 
                 <table id="example" class="table table-striped table-bordered">
-                  
+
                   <thead>
                     <tr>
                       <th class="text-center">Nª</th>
@@ -118,7 +126,7 @@ include "include/verificar_sesion.php";
 
                   <tbody>
                     <?php
-                    $consulta = "select DISTINCT id,documento,llabeglobal,email,nombrecliente,pais, registroapp,noregistrado,pointz, nombreusaurio,emailusuario,telefono,antencion,estado,ip,fechai,fechau,puntuacion,preguntas,dato1,dato2,dato3,dato4,dato5,tipo,pluss,veces, nivel from clientestra WHERE documento != '' ORDER BY puntuacion DESC";
+                    $consulta = "select * FROM clientestra WHERE documento != '' ORDER BY puntuacion DESC";
                     $resultado = $dblink->query($consulta);
                     $pos = 0;
                     while ($row = $resultado->fetch_assoc()) {
@@ -167,9 +175,6 @@ include "include/verificar_sesion.php";
           </div>
         </div>
       </div>
-      <!-- /page content -->
-      <!-- footer content -->
-      <!-- /footer content -->
     </div>
   </div>
 

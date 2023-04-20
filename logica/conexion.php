265bb1c0f623f8a1dfb071f6d41dec7f4b4f0b92
efrael <efrael2001@gmail.com>
2023-04-20 03:22:39 +0200

    s

--- logica/conexion.php
+++ logica/conexion.php
@@ -4,19 +4,26 @@
    $user = "u960900126_triviaeuro";
    $pass = "Cocorilow.1";
    $database = "u960900126_eurotrivia";
-   $dblink = mysqli_connect($host, $user, $pass, $database);
-
-   if($dblink){
-      }else{echo "NO ESTA CONECTADO";
-   }
-   */
-   $host = "localhost";
+     $host = "localhost";
   //  $user = "u960900126_Efrael";
    $user = "root";
   //  $pass = "AdminTransitionRun2023";
    $pass = "";
   //  $database = "u960900126_transitionsrun";
    $database = "transitionrun";
+   $dblink = mysqli_connect($host, $user, $pass, $database);
+
+   if($dblink){
+      }else{echo "NO ESTA CONECTADO";
+   }
+   */
+  $host = "localhost";
+  $user = "desarro1_transitionrun";
+  // $user = "root";
+  $pass = "_BTkOL.R^PfP";
+  // $pass = "";
+  $database = "desarro1_transitionrun";
+  // $database = "transitionrun";
    $dblink = mysqli_connect($host, $user, $pass, $database);
    if($dblink){
     //  echo "conectadosss";
